0c3151b41eedbab5628e2dc4533d9c6f39b5bbca
Stas Anishchenko <anischenko_stas@mail.ru>
2020-06-01 19:33:49 +0500

    - change package name

--- app/src/main/java/com/eburg_soft/companiesapp/common/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.eburg_soft.companiesapp.common
-
-object Constants {
-    const val BASE_URL = "http://megakohz.bget.ru"
-    const val MAX_READ_TIMEOUT: Long = 5
-    const val MAX_CONNECT_TIMEOUT: Long = 5
-}
